dd1bf1f8f9704c33c363848633c9ac9f6c8eb107
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-10-03 17:31:36 +0200

    Add missing updates in files (function.js, script.js, index.html)

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -25,30 +26,63 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
   };
-
+  
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active',
+    },
+    // CODE ADDED END
   };
-
+  
   const settings = {
     amountWidget: {
       defaultValue: 1,
-      defaultMin: 0,
-      defaultMax: 10,
-    }
+      defaultMin: 1,
+      defaultMax: 9,
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
+    },
+    // CODE ADDED END
   };
-
+  
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
   };
 
   class Product{
@@ -267,18 +301,34 @@
       const thisCart = this;
 
       thisCart.products = [];
-
+    
       thisCart.getElements(element);
-
+      thisCart.initActions();
+    
       console.log('this cart: ', thisCart);
     }
 
     getElements(element){
+      
+      const thisCart = this;
+  
+      thisCart.dom = {};
+  
+      thisCart.dom.wrapper = element;
+      console.log('element:', element);
+  
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(
+        select.cart.toggleTrigger
+      );
+    }
+
+    initActions(){
       const thisCart = this;
 
-      thisCart.dom = {};
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
 
-      thisCart.dom.wrapper = element;
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
@@ -287,6 +337,13 @@
   //}
 
   const app = {
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     initMenu: function(){
       const thisApp = this;
 
@@ -296,11 +353,14 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
-
-    initData: function(){
+   
+    initCart: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      console.log('tworzenie cart elem');
+      console.log(cartElem);
+      thisApp.cart = new Cart(cartElem);
     },
 
     init: function(){
@@ -314,6 +374,7 @@
       thisApp.initData();
       console.log('this app data:', thisApp.data);
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
