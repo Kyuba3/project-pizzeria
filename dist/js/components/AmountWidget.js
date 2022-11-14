fbd0398bcaeae7ad82199c8d561a557e9146eeed
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-11-14 11:31:08 +0100

    Fixing amount widget

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,19 +1,22 @@
-import {settings, select} from '../settings.js';
+import {select, settings} from '../settings.js';
 import BaseWidget from './BaseWidget.js';
 
 class AmountWidget extends BaseWidget{
   constructor(element){
     super(element, settings.amountWidget.defaultValue);
-    
     const thisWidget = this;
 
     thisWidget.getElements(element);
     thisWidget.initActions(thisWidget.value);
+    thisWidget.value = thisWidget.dom.input.value;
+
+    // console.log('AmountWidget:', thisWidget);
+    // console.log('constructor arguments:', element);
   }
-    
+
   getElements(){
     const thisWidget = this;
-
+    
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
     thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
@@ -35,9 +38,9 @@ class AmountWidget extends BaseWidget{
     const thisWidget = this;
 
     thisWidget.dom.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value(thisWidget.dom.input.value);
     });
-      
+
     thisWidget.dom.linkDecrease.addEventListener('click', function(event){
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
